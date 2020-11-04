6ec3b059924d8a73a08afe4f3cdb6dee5597c338
riie02 <riie0220@icloud.com>
2020-11-04 21:17:09 +0900

    RspecとFactoryBotの準備とユーザー情報をFactoryBotで指定

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,13 +7,13 @@ class User < ApplicationRecord
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 
   validates :nickname,            presence: true
-  validates :family_name,         presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/}
-  validates :first_name,          presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/}
-  validates :family_name_kana,    presence: true, format: { with: /\A([ァ-ン]|ー)+\z/}
-  validates :first_name_kana,     presence: true, format: { with: /\A([ァ-ン]|ー)+\z/}
+  validates :family_name,         presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
+  validates :first_name,          presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
+  validates :family_name_kana,    presence: true, format: { with: /\A([ァ-ン]|ー)+\z/ }
+  validates :first_name_kana,     presence: true, format: { with: /\A([ァ-ン]|ー)+\z/ }
   validates :birth_day,           presence: true
 
   validates :email, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
-  validates :password, format: { with: /\A[a-zA-Z0-9]+\z/}
+  validates :password, format: { with: /\A[a-zA-Z0-9]+\z/ }
   
 end
