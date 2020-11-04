0966737ec9bca76e29eefa11515a43f864a009f6
riie02 <riie0220@icloud.com>
2020-11-04 15:30:34 +0900

    ユーザー管理機能

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,11 +4,16 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :nickname,           presence: true
-  validates :family_name,        presence: true
-  validates :first_name,         presence: true
-  validates :family_name_kana,   presence: true
-  validates :first_name_kana,    presence: true
-  validates :birth_day,          presence: true
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 
+  validates :nickname,            presence: true
+  validates :family_name,         presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/}
+  validates :first_name,          presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/}
+  validates :family_name_kana,    presence: true, format: { with: /\A([ァ-ン]|ー)+\z/}
+  validates :first_name_kana,     presence: true, format: { with: /\A([ァ-ン]|ー)+\z/}
+  validates :birth_day,           presence: true
+
+  validates :email, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
+  validates :password, format: { with: /\A[a-zA-Z0-9]+\z/}
+  
 end
