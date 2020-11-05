820a885d7d53272fc801b13a99519c38e57c0a88
riie02 <riie0220@icloud.com>
2020-11-05 20:05:59 +0900

    バリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -9,16 +9,24 @@ class Item < ApplicationRecord
   belongs_to :shipping_area
   belongs_to :shipping_day
 
-  #with_options presence: true do
-    #validates :name,       
-    #validates :description,
-    #validates :category_id
-    #validates :condition_id
-    #validates :shipping_burden_id
-    #validates :shipping_area_id
-    #validates :shipping_day_id
-    #validates :price, numericality: { only_integer: true, greater_than: 299, less_than: 9,999,999}, format: {with: /\A[0-9]+\z/}
-    #validates :category
-  #end
+  with_options presence: true do
+    validates :name,       
+    validates :description,
+    validates :category_id
+    validates :condition_id
+    validates :shipping_burden_id
+    validates :shipping_area_id
+    validates :shipping_day_id
+    validates :price, numericality: { only_integer: true, greater_than: 299, less_than: 9,999,999, messeges:"Price Out of setting range"}, format: {with: /\A[0-9]+\z/, messeges:"Price Half-width number"}
+    validates :image
+  end
+
+  with_options numericality: { other_than: 1, messeage:"Select" } do
+    validates :category_id
+    validates :condition_id
+    validates :shipping_burden_id
+    validates :shipping_area_id
+    validates :shipping_day_id
+  end
 
 end
