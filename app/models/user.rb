6c701776eb97b47f48c7bdd5e2ca9e1d159edc05
riie02 <riie0220@icloud.com>
2020-11-05 12:27:59 +0900

    with_options presence: truを使用しコードを修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,13 +6,17 @@ class User < ApplicationRecord
 
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i.freeze
 
-  validates :nickname,            presence: true
-  validates :family_name,         presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
-  validates :first_name,          presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
-  validates :family_name_kana,    presence: true, format: { with: /\A([ァ-ン]|ー)+\z/ }
-  validates :first_name_kana,     presence: true, format: { with: /\A([ァ-ン]|ー)+\z/ }
-  validates :birth_day,           presence: true
+  with_options presence: true do
+    validates :nickname
+    validates :family_name,      format: { with: /\A[ぁ-んァ-ン一-龥]/ }
+    validates :first_name,       format: { with: /\A[ぁ-んァ-ン一-龥]/ }
+    validates :family_name_kana, format: { with: /\A([ァ-ン]|ー)+\z/ }
+    validates :first_name_kana,  format: { with: /\A([ァ-ン]|ー)+\z/ }
+    validates :birth_day
+  end
+
 
   validates :email, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
   validates :password, format: { with: /\A[a-zA-Z0-9]+\z/ }
+
 end
