178a8523dfa9eb0162e02793b4be7b25f921d439
riie02 <riie0220@icloud.com>
2020-11-08 13:34:53 +0900

    itemモデルの単体テストの修正（数値のためシングルクウォーテーションを外した）

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -29,31 +29,31 @@ RSpec.describe Item, type: :model do
     end
 
     it 'category_idが1だった場合登録できない' do
-      @item.category_id = "1"
+      @item.category_id = 1
       @item.valid?
       expect(@item.errors.full_messages).to include("Category must be other than 1")
     end
 
     it 'condition_idが1だった場合登録できない' do
-      @item.condition_id = "1"
+      @item.condition_id = 1
       @item.valid?
       expect(@item.errors.full_messages).to include("Condition must be other than 1")
     end
 
     it 'shipping_burden_idが1だった場合登録できない' do
-      @item.shipping_burden_id = "1"
+      @item.shipping_burden_id = 1
       @item.valid?
       expect(@item.errors.full_messages).to include("Shipping burden must be other than 1")
     end
 
     it 'shipping_area_idが1だった場合登録できない' do
-      @item.shipping_area_id = "1"
+      @item.shipping_area_id = 1
       @item.valid?
       expect(@item.errors.full_messages).to include("Shipping area must be other than 1")
     end
 
     it 'shipping_day_idが1だった場合登録できない' do
-      @item.shipping_day_id = "1"
+      @item.shipping_day_id = 1
       @item.valid?
       expect(@item.errors.full_messages).to include("Shipping day must be other than 1")
     end
@@ -65,13 +65,13 @@ RSpec.describe Item, type: :model do
     end
 
     it '価格が299円以下では登録できないこと' do
-      @item.price = '299'
+      @item.price = 299
       @item.valid?
       expect(@item.errors.full_messages).to include
     end
 
     it '価格が10,000,000以上では登録できないこと' do
-      @item.price = '10,000,000'
+      @item.price = 10,000,000
       @item.valid?
       expect(@item.errors.full_messages).to include
     end
