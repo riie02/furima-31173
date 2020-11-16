f55d7ad317b3f26b189ec1c48cf189a2d2fe50a0
riie02 <riie0220@icloud.com>
2020-11-16 16:02:15 +0900

    アソシエーションの追記

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :items
+  has_many :orders
 
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i.freeze
 
