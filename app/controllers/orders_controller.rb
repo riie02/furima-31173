428b9cfdccf86d88a2304ed5924b92d2ca2dd0aa
riie02 <riie0220@icloud.com>
2020-11-18 20:39:05 +0900

    決済処理の実装

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,7 +2,7 @@ class OrdersController < ApplicationController
   before_action :authenticate_user!, only: [:index]
   before_action :set_item, only: [:index, :create]
   before_action :purchased, only: [:index]
-
+  
   def index
     @order_addresses_form = OrderAddressesForm.new
     if current_user == @item.user
@@ -13,6 +13,7 @@ class OrdersController < ApplicationController
   def create
     @order_addresses_form = OrderAddressesForm.new(order_params)
     if @order_addresses_form.valid?
+      pay_item
       @order_addresses_form.save
       redirect_to root_path
     else
@@ -23,7 +24,7 @@ class OrdersController < ApplicationController
   private
 
   def order_params
-    params.require(:order_addresses_form).permit(:postal_code, :prefecture_id, :city, :street_address, :building_name, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id])
+    params.require(:order_addresses_form).permit(:postal_code, :prefecture_id, :city, :street_address, :building_name, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
   end
 
   def set_item
@@ -35,5 +36,15 @@ class OrdersController < ApplicationController
       redirect_to root_path
     end
   end
+
+  def pay_item
+    Payjp.api_key = "sk_test_82a224a7c25e56512200f5af"
+    Payjp::Charge.create(
+      amount: @item[:price],
+      card: order_params[:token],
+      currency: 'jpy'
+    )
+  end
+    
 
 end
