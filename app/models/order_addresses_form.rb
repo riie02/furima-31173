bc96d5d68bc97e0fdfca479f29be91d3f50d83da
riie02 <riie0220@icloud.com>
2020-11-18 20:38:00 +0900

    クレジットカード情報のトークン化の実装

--- app/models/order_addresses_form.rb
+++ app/models/order_addresses_form.rb
@@ -1,6 +1,6 @@
 class OrderAddressesForm
   include ActiveModel::Model
-  attr_accessor :postal_code, :prefecture_id, :city, :street_address, :building_name, :phone_number, :user_id, :item_id
+  attr_accessor :postal_code, :prefecture_id, :city, :street_address, :building_name, :phone_number, :user_id, :item_id, :token
 
   with_options presence: true do
     validates :postal_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: "Postal code is invalid. Include hyphen(-)" }
@@ -8,6 +8,7 @@ class OrderAddressesForm
     validates :city
     validates :street_address
     validates :phone_number, format: { with: /\A\d{10}$|^\d{11}\z/, message: "Phone number Input only number" }
+    validates :token, presence: true
   end
 
   def save
