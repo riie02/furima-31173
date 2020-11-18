ab34474fec4ae56b3a2447a7cf4921ff7978c591
riie02 <riie0220@icloud.com>
2020-11-18 12:02:02 +0900

    order_addresses_forのFactoryBot作成

--- spec/factories/order_addresses_forms.rb
+++ spec/factories/order_addresses_forms.rb
@@ -1,5 +1,10 @@
 FactoryBot.define do
   factory :order_addresses_form do
-    
+    postal_code { '123-4567' }
+    prefecture_id { Faker::Number.between(from: 2, to: 48) }
+    city { '東京都' }
+    street_address { '1-1 '}
+    building_name { '東京ハイツ' }
+    phone_number { '09012345678' }
   end
 end
